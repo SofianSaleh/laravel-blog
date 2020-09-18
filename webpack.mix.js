6815ed865411b24e7c4565a5cffe345503059c74
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-18 11:35:16 +0200

    :fire: adding vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js").styles(
+    ["public/css/vendor/normalize.css", "public/css/vendor/videojs.css"],
+    "public/css/all.css"
+);
