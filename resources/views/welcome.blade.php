e59aec61ccf70eb6b26e20ffd08f5da2086c630c
SofianSaleh <sofiane.saleh11@gmail.com>
2020-09-18 12:08:07 +0200

    :fire: adding vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/grid.min.css">
         <link rel="stylesheet" href="/all.css">
         
     </head>
